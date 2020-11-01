c6ca1adb88ee4f83064fc518cc7c8c35f01ca4c2
Skoomaa <lavigne.felix@carrefour.cegepvicto.ca>
2020-11-01 18:16:23 -0500

    hello

---
+++ script/main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+onready var joueur = get_node('Player')
+onready var plateformeFin = get_node('Plateforme9')
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	pass
+	
+
